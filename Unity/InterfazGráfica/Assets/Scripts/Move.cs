5a5777c1705af337a0259b97ac738db08707b94e
Luis Delgado <62965678+Tiquillo@users.noreply.github.com>
2020-11-25 01:15:43 -0600

    vugz

--- Unity/InterfazGráfica/Assets/Scripts/Move.cs
+++ Unity/InterfazGráfica/Assets/Scripts/Move.cs
@@ -4,57 +4,130 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    // public Transform transform;
-    // public GameObject gameObject;
-    public bool isGrounded;
-    public int pj;
+    private Transform transform;
+    
+    public int player;
     private Rigidbody rb;
+    private Animator animator;
+
+    // Moving
+    private Vector3 lastPosition;
+    private float zSpeed;
+    private bool inAir;
+    private bool canSecondJump;
+
+    bool fall;
 
     private void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        animator = this.GetComponent<Animator>();
+        transform = this.GetComponent<Transform>();
+        lastPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (pj == 0)
+        ResetTriggers();
+        Land();
+
+        if (player == 0)
         {
-            if (Input.GetKey(KeyCode.A))
-            {
-                this.transform.Translate(Vector3.left * 4 * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                this.transform.Translate(Vector3.right * 4 * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.W))
-            {
-                Jump();
-            }
-        } else if (pj == 1)
-        {
-            if (Input.GetKey(KeyCode.J))
-            {
-                this.transform.Translate(Vector3.left * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.L))
-            {
-                this.transform.Translate(Vector3.right * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.I))
-            {
-                Jump();
-            }
+            if (Input.GetKeyDown(KeyCode.W)) { Jump(); }
+            else if (Input.GetKey(KeyCode.D)) { Run(); }
+            else if (Input.GetKey(KeyCode.A)) { RunBackwards(); }
+            else { NotMoving(); }
         }
-        this.transform.rotation = new Quaternion(0, 0, 0, 0);
+        else if (player == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.I)) { Jump(); }
+            else if (Input.GetKey(KeyCode.J)) { Run(); }
+            else if (Input.GetKey(KeyCode.L)) { RunBackwards(); }
+            else { NotMoving(); }
+        }
+
+        CheckInAir();
+        
+    }
+
+    void Run()
+    {
+        if (!inAir)
+        {
+            if (!animator.GetBool("running")) {
+                Debug.Log("asd");
+                animator.SetBool("running", true);
+                animator.SetTrigger("runningT");
+            }
+
+            transform.Translate(Vector3.forward * 4 * Time.deltaTime);
+        }
+    }
+
+    void RunBackwards()
+    {
+        if (!inAir)
+        {
+            if (!animator.GetBool("backwards")) { 
+                animator.SetBool("backwards", true);
+                animator.SetTrigger("backwardsT");
+            }
+
+            transform.Translate(Vector3.back * Time.deltaTime);
+        }
     }
 
     void Jump()
     {
-        if (isGrounded)
+        if (inAir || canSecondJump)
         {
-            this.GetComponent<Rigidbody>().AddForce(Vector3.up * 3000);
+            rb.AddForce(Vector3.up * 3000);
+            inAir = false;
+            animator.SetBool("inAir", true);
+            animator.SetTrigger("jump");
+
+            if (!inAir) { canSecondJump = false; }
+        }
+    }
+
+    void NotMoving()
+    {
+        if (animator.GetBool("running")) { animator.SetBool("running", false); }
+        if (animator.GetBool("backwards")) { animator.SetBool("backwards", false); }
+    }
+
+    void ResetTriggers()
+    {
+        animator.ResetTrigger("jump");
+    }
+
+    void CheckInAir()
+    {
+        zSpeed = (transform.position.z - lastPosition.z) / Time.deltaTime;
+        lastPosition = transform.position;
+
+        inAir = (zSpeed > 3f || zSpeed < -3f);
+        if (!inAir)
+        {
+            canSecondJump = true;
+            animator.SetBool("inAir", false);
+        }
+    }
+
+    void Land()
+    {
+        if (fall)
+        {
+            transform.Translate(Vector3.down * 9.81f * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            fall = true;
         }
     }
 
@@ -62,17 +135,7 @@ public class Move : MonoBehaviour
     {
         if (other.gameObject.tag == "Ground")
         {
-            isGrounded = true;
-            this.GetComponent<Animator>().SetBool("inAir", false);
-        }
-    }
-
-    void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.tag == "Ground")
-        {
-            isGrounded = false;
-            this.GetComponent<Animator>().SetBool("inAir", true);
+            fall = true;
         }
     }
 }
