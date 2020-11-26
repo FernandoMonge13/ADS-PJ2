74d79cf23eddf110004a735b333a24a868944072
CAMANEM <camanem10@hotmail.com>
2020-11-25 22:17:53 -0600

    Change to Maven

---
+++ src/main/java/Message.java
@@ -0,0 +1,14 @@
+public class Message {
+
+    public String salute;
+
+    public void setSalute(String salute)
+    {
+        this.salute = salute;
+    }
+
+    public String getSalute()
+    {
+        return this.salute;
+    }
+}
